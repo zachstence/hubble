3638dc4915f261a3b00afefe42d20765fd42d404
Zachary Stence <zachstence@gmail.com>
2022-01-24 09:50:37 -0600

    refactor: remove UploadConfig for now

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
 	import Item from "./Item.svelte";
 	import {config} from "./store";
-import UploadConfig from "./UploadConfig.svelte";
 </script>
 
 <main>
 	{#each $config.items as item}
 		<Item name={item.name} link={item.link} />
 	{/each}
-	<UploadConfig />
 </main>
 
 
--- src/store.ts
+++ src/store.ts
@@ -1,22 +1,29 @@
 import { writable } from "svelte/store";
+import type { Writable } from "svelte/store";
 import type { Config } from "./types";
 
 const defaultConfig: Config = {
     items: [],
 };
 
-const createConfigStore = () => {
+type ConfigStore = Writable<Config> & {init: () => void}
+
+const createConfigStore = (): ConfigStore => {
     const store = writable<Config>(defaultConfig);
 
     return {
         ...store,
         init: async () => {
-            const res = await fetch("config.json");
-            if (res.status === 200) {
-                const json = await res.json();
-                store.set(json);
+            try {
+                const res = await fetch("config.json");
+                if (res.status === 200) {
+                    const json = await res.json();
+                    store.set(json);
+                }
+            } catch (err) {
+                console.error(`Unable to load config: ${err}`);
             }
-        }
+        },
     }
 }
 
